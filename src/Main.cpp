04a7e5e5ac043a8e0bcbe07639a147be4cd13d4b
Sam <doogly1@gmail.com>
2014-02-25 20:20:09 +0000

    Project configuration, Compiling and linking with Irrlicht

---
+++ src/Main.cpp
@@ -0,0 +1,17 @@
+/*===============================================================================
+  Author:   Sam Serrels
+  Purpose:  Entry and exit point for game application.
+===============================================================================*/
+
+#pragma comment(lib,"Irrlicht")
+
+/*
+#include "Engine.h"
+#include "Game.h"
+#include "PhysicsDemo.h"
+#include <iostream>
+*/
+
+int main(){
+	return 0;
+}
