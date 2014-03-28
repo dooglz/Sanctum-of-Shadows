879e23429116e2594c8aa48cf23b0bdd0a0ccf57
Sam Serrels <doogly1@gmail.com>
2014-03-28 13:35:13 +0000

    Removed junk code.

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -8,24 +8,20 @@ Obstacle::Obstacle (const irr::core::vector3df& position, const irr::core::vecto
 
 	//scene node
 	loadContent();
-	irr::scene::IMesh* m = GameEngine::engine.getDevice()->getSceneManager()->getMesh("models/cube1.obj");
-	//GameEngine::engine.getDevice()->getSceneManager()->getMeshManipulator()->scale(m,size);
-	//GameEngine::engine.getDevice()->getSceneManager()->getMeshManipulator()->recalculateNormals(m,0,0);
-	GameEngine::engine.getDevice()->getSceneManager()->getMeshManipulator()->recalculateTangents(m,1,0,0);
-	 //m = GameEngine::engine.getDevice()->getSceneManager()->getMeshManipulator()->createMeshWithTangents(m);
 
-	_node = GameEngine::engine.getDevice()->getSceneManager()->addMeshSceneNode(m);
-	//_node->setMaterialFlag(irr::video::EMF_FOG_ENABLE, true);
-	_node->setMaterialFlag(irr::video::EMF_LIGHTING, true);
+	_node = GameEngine::engine.getDevice()->getSceneManager()->addMeshSceneNode(_mesh);
+	
 	_node->setMaterialFlag(irr::video::EMF_NORMALIZE_NORMALS, true);
+	_node->setMaterialFlag(irr::video::EMF_FOG_ENABLE, true);
+	_node->setMaterialType(irr::video::EMT_NORMAL_MAP_SOLID);
+	_node->setMaterialFlag(irr::video::EMF_LIGHTING, true);
+
 	_node->setMaterialTexture(0, _texture);
 	_node->setPosition(position);
-
-	_node->setMaterialType(irr::video::EMT_NORMAL_MAP_SOLID);
+	_node->setScale(size);
+	
 	_node->setMaterialTexture(1, GameEngine::engine.getDevice()->getVideoDriver()->getTexture("textures/tex_cobble_1024_NRM.jpg"));
-		_node->setScale(size);
-		_node->setMaterialFlag(irr::video::EMF_NORMALIZE_NORMALS, true);
-	GameEngine::engine.getDevice()->getSceneManager()->getMeshManipulator()->recalculateNormals(m,true,false);
+	
 	//Collision
 	_rigidBody = createBoundingBoxRB( _node, irr::core::vector3df(0,size.Y*0.5f*_node->getTransformedBoundingBox().getExtent().Y,0) );
 	GameEngine::Physics::world->addRigidBody(_rigidBody);
