8d9fede8cf1657b47bd12d2613689518ea95ecd6
Sam Serrels <doogly1@gmail.com>
2014-03-29 14:49:56 +0000

    Menu Logo

--- src/Menu_Scene.cpp
+++ src/Menu_Scene.cpp
@@ -3,6 +3,8 @@
 #include "Game.h"
 
 irr::gui::IGUIImage* img;
+irr::scene::ICameraSceneNode* menucam;
+irr::scene::ISceneNode* logoNode;
 
 Menu_Scene::Menu_Scene():GameState("menu")
 {
@@ -24,6 +26,19 @@ bool Menu_Scene::loadContent()
 void Menu_Scene::initialize()
 {
 	std::cout << "Menu_Scene initialize" << std::endl;
+		irr::scene::ISceneManager* smgr = GameEngine::engine.getDevice()->getSceneManager();
+
+	logoNode = smgr->addBillboardSceneNode(0, irr::core::dimension2d<irr::f32>(1280, 163));
+	//logoNode->setMaterialFlag(irr::video::EMF_TEXTURE_WRAP, true);
+	logoNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	logoNode->setMaterialFlag(irr::video::EMF_NORMALIZE_NORMALS, true);
+	//logoNode->setMaterialType(irr::video::EMT_TRANSPARENT_ADD_COLOR);
+	logoNode->setMaterialTexture(0, GameEngine::engine.getDevice()->getVideoDriver()->getTexture("textures/Logo1.jpg"));
+//	logoNode->getMaterial(0).getTextureMatrix(0).setTextureScale(5.0f,5.0f);
+
+	menucam = smgr->addCameraSceneNode(0,irr::core::vector3df(0,0,500),irr::core::vector3df(0,0,0));
+	irr::scene::ILightSceneNode* workLight = smgr->addLightSceneNode(0, irr::core::vector3df(0,0,0), irr::video::SColorf(1.0f, 0.6f, 0.7f, 1.0f), 10000.0f);
+
 	loadContent();
 }
 
